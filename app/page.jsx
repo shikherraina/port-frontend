4f199d00c9bd62aedb9528e371a89f9cf814007f
shikherraina <shikhraina07@gmail.com>
2026-01-18 21:54:01 +0530

    Configuration Set,Backend URL from Secrets

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,18 @@
+"use client";
+import { useEffect } from "react";
+export default function Page() {
+  const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-export default function page() {
-  return (
-    <div>Work in progress...</div>
-  )
+  useEffect(() => {
+    if (!BACKEND_URL) {
+      console.warn("Backend URL not set");
+      return;
+    }
+    fetch(`${BACKEND_URL}/health`)
+      .then((res) => res.json())
+      .then((data) => console.log("Backend health:", data))
+      .catch((err) => console.error("Error fetching backend health:", err));
+  }, [BACKEND_URL]);
+
+  return <div>Work in progress...</div>;
 }
